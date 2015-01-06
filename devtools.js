7eea24de89767ea04eb55549f840c0750d7c2b75
zenmaddy <mjha@zenefits.com>
2015-01-05 23:58:17 -0800

    minor

--- devtools.js
+++ devtools.js
@@ -3,4 +3,8 @@
 // chrome.extension.*
 
 // Create a tab in the devtools area
-chrome.devtools.panels.create("DemoPanel", "toast.png", "panel.html", function(panel) {});
+chrome.devtools.panels.create("MyDemoPanel", 
+	null,				// No Icon Path
+	"panel.html", 
+	function(panel) {}
+	);
