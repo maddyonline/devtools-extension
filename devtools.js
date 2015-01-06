d817273fb97dcdfcd0f738683e9d4ddedb1ac0e4
zenmaddy <mjha@zenefits.com>
2015-01-06 00:38:57 -0800

    finally nearing completion

--- devtools.js
+++ devtools.js
@@ -3,6 +3,12 @@
 // chrome.extension.*
 
 // Listen to all network requests
+chrome.devtools.network.onRequestFinished.addListener(
+          function(har) {
+            chrome.experimental.devtools.console.addMessage(
+                chrome.experimental.devtools.console.Severity.Warning,
+                har.request.method + ' ' + har.request.url);
+      });
 // Create a tab in the devtools area
 chrome.devtools.panels.create("MyDemoPanel", 
 	null,				// No Icon Path
