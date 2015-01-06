e3e87d5e14020438ced68ed985dbf3d2ac7cc440
zenmaddy <mjha@zenefits.com>
2015-01-06 01:15:31 -0800

    nearing completion

--- devtools.js
+++ devtools.js
@@ -4,11 +4,17 @@
 
 // Listen to all network requests
 chrome.devtools.network.onRequestFinished.addListener(
-          function(har) {
-            chrome.experimental.devtools.console.addMessage(
-                chrome.experimental.devtools.console.Severity.Warning,
-                har.request.method + ' ' + har.request.url);
-      });
+	function(request) {
+		var URL = "https://aa.com/?status=O";
+		if(request.request.url != URL)
+			return;
+		request.getContent(
+			function(content, encoding) {
+          		chrome.experimental.devtools.console.addMessage(
+          			chrome.experimental.devtools.console.Severity.Error,
+          			'NinjaTool:' + typeof(JSON.parse(content)));
+          	});
+	});
 // Create a tab in the devtools area
 chrome.devtools.panels.create("MyDemoPanel", 
 	null,				// No Icon Path
